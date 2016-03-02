f09a31ff9e84ccb280d8206f17abf0d715e225af
Yujie Qin <yujieqin@google.com>
2016-03-02 16:54:58 +0100

    Integrate integer overflow changes from security branch
    Replace the limits from stdint.h by the limit from std::numeric_limits<>.

--- source/dng_safe_arithmetic.h
+++ source/dng_safe_arithmetic.h
@@ -1,20 +1,4 @@
-/*
- *
- * Copyright (C) 2015 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+// Copyright 2015 Google Inc. All Rights Reserved.
 // Functions for safe arithmetic (guarded against overflow) on integer types.
 
 #ifndef __dng_safe_arithmetic__
@@ -213,9 +197,10 @@ static void ConvertUnsigned(TSrc src, TDest *dest) {
   *dest = converted;
 }
 
-// Returns the result of converting val to an int32_t using truncation if val is
-// in range of int32_t values. Otherwise, throws a dng_exception with error code
-// dng_error_unknown.
+// Returns the result of converting val to the result type using truncation if
+// val is in range of the result type values. Otherwise, throws a dng_exception
+// with error code dng_error_unknown.
 std::int32_t ConvertDoubleToInt32(double val);
+std::uint32_t ConvertDoubleToUint32(double val);
 
 #endif  // __dng_safe_arithmetic__
